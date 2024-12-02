d7d8274a822d65ffcb3df2d20fdfa7b808cee58c
Artur <belokurovarturka@gmail.com>
2024-12-02 13:29:54 +0300

    1st task update

---
+++ src/main/java/Task1/CustomGraphics/StandardOvalDrawer.java
@@ -0,0 +1,11 @@
+package Task1.CustomGraphics;
+
+import java.awt.*;
+
+public class StandardOvalDrawer implements OvalDrawer{
+
+    @Override
+    public void drawOval(Graphics2D g2d, int x, int y, int width, int height) {
+        g2d.drawOval(x,y,width,height);
+    }
+}
